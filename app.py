2bcaf93c759c776d33002692d535655d9e536800
Ayaan Sachdeva <ayaansachdeva1000@gmail.com>
2023-08-20 21:43:04 -0400

    added html

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html')
+
+@app.route("/initiatives")
+def initiatives():
+    return render_template('initiatives.html')
